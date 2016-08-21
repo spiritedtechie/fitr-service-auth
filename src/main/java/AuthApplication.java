10f631be4cc86ea7de7c0cf2d61c45df77454432
Vijay Patel <vijay@spiritedtechie.com>
2016-08-21 09:41:16 +0100

    Cleanup - removed (moved to user profile service) auth filter for resources.

--- src/main/java/AuthApplication.java
+++ src/main/java/AuthApplication.java
@@ -1,17 +1,10 @@
-import auth.AuthTokenAuthenticator;
-import auth.TokenAuthFilter;
-import auth.UserRoleAuthorizer;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import dao.UserDao;
 import dao.UserDaoImpl;
 import io.dropwizard.Application;
-import io.dropwizard.auth.AuthDynamicFeature;
-import io.dropwizard.auth.AuthValueFactoryProvider;
 import io.dropwizard.setup.Environment;
-import model.User;
 import org.glassfish.jersey.filter.LoggingFilter;
-import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import resource.AuthenticateResource;
 import resource.SignupResource;
 import service.AuthenticateService;
@@ -50,23 +43,6 @@ public class AuthApplication extends Application<AuthConfiguration> {
         // register resources
         environment.jersey().register(signupResource);
         environment.jersey().register(authenticateResource);
-
-        // register auth framework
-//        registerAuth(configuration, environment, dao);
-    }
-
-    private void registerAuth(AuthConfiguration configuration, Environment environment, UserDao dao) {
-
-        final TokenAuthFilter<User> tokenAuthFilter = new TokenAuthFilter.Builder<User>()
-                .setAuthorizer(
-                        new UserRoleAuthorizer()
-                ).setAuthenticator(
-                        new AuthTokenAuthenticator(configuration.getAuthTokenSecret(), dao)
-                ).buildAuthFilter();
-
-        environment.jersey().register(new AuthDynamicFeature(tokenAuthFilter));
-        environment.jersey().register(RolesAllowedDynamicFeature.class);
-        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
     }
 
     private DB setupMongoDB(AuthConfiguration configuration, Environment environment) throws UnknownHostException {
